760476dcfa49e3c0afbd8af3a15198a54ce25607
rventurini13 <re18997005140@gmail.com>
2025-06-11 08:41:27 -0400

    Update index.js

--- index.js
+++ index.js
@@ -15,4 +15,15 @@ let isReady = false;
 const client = new Client({
   authStrategy: new LocalAuth({ dataPath: './sessions' }),
   puppeteer: {
-    headless: true,
+  headless: true,
+  args: [
+    '--no-sandbox',
+    '--disable-setuid-sandbox',
+    '--disable-dev-shm-usage',
+    '--disable-accelerated-2d-canvas',
+    '--no-first-run',
+    '--no-zygote',
+    '--single-process',
+    '--disable-gpu'
+  ]
+}
